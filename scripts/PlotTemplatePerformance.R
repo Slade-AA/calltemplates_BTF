f0000f7b893fd3f5a36efedb3ddd624b873c8f4c
Slade Allen-Ankins <jc696551@ad.jcu.edu.au>
2021-12-13 10:16:35 +1000

    Added plots of template performance and added workflow to generate readme

--- scripts/PlotTemplatePerformance.R
+++ scripts/PlotTemplatePerformance.R
@@ -5,6 +5,9 @@ library(tidyverse)
 
 TemplatePerformance <- readRDS("outputs/TemplatePerformance.rds")
 
+
+# Template performance per recording --------------------------------------
+
 #Handling of edge cases for per recording performance measures:
 #if TP + FN == 0 then Recall == NA
 #if FP == 0 && TP == 0 then FDR == 0
@@ -32,3 +35,45 @@ ggplot(data = PerformanceSummary[PerformanceSummary$Template == '1,2,3',], aes(x
   geom_point() +
   scale_x_continuous(limits = c(0,1)) +
   theme_bw()
+
+
+# Performance over all recordings -----------------------------------------
+
+PerformanceSummary <- TemplatePerformance %>% 
+  group_by(Template, ScoreCutoff) %>%
+  summarise(TruePos = sum(TruePos),
+            TrueNeg = sum(TrueNeg),
+            FalsePos = sum(FalsePos),
+            FalseNeg = sum(FalseNeg)) %>% 
+  mutate(Recall = TruePos/(TruePos+FalseNeg),
+         FDR = FalsePos/(TruePos+FalsePos),
+         Precision = TruePos/(TruePos+FalsePos))
+
+individualTemplates <- grep("template*", levels(PerformanceSummary$Template), value = TRUE)
+
+Plot_SingleTemplates <- ggplot(data = PerformanceSummary[PerformanceSummary$Template %in% individualTemplates,], aes(x = Recall, y = Precision, 
+                                                                                                                     group = Template, colour = Template)) + 
+  geom_line(lwd = 1) +
+  scale_x_continuous(limits = c(0,1)) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(nrow = 3))
+
+ggsave(filename = "outputs/figures/Plot_SingleTemplates.png",
+       plot = Plot_SingleTemplates,
+       height = 15, width = 20, units = "cm", dpi = 800)
+
+
+combinationTemplates <- grep("template*", levels(PerformanceSummary$Template), value = TRUE, invert = TRUE)
+
+Plot_MultipleTemplates <- ggplot(data = PerformanceSummary[PerformanceSummary$Template %in% combinationTemplates,], aes(x = Recall, y = Precision, 
+                                                                                                                        group = Template, colour = Template)) + 
+  geom_line(lwd = 1) +
+  scale_x_continuous(limits = c(0,1)) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(nrow = 3))
+
+ggsave(filename = "outputs/figures/Plot_MultipleTemplates.png",
+       plot = Plot_MultipleTemplates,
+       height = 15, width = 20, units = "cm", dpi = 800)
